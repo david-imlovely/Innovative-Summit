2344d886d9b48fe3f817e0d2fc371237204a0f41
David <davidtanimowo01@gmail.com>
2024-09-21 17:15:48 +0100

    innovative summit new look

--- index.js
+++ index.js
@@ -21,37 +21,52 @@ function sendEmail() {
   }).then((message) => alert(message));
 }
 
-// code for changing navbar bg color
+document.getElementById("hamburger").onclick = function toggleMenu() {
+  const navToggle = document.getElementsByClassName("toggle");
+  for (let i = 0; i < navToggle.length; i++) {
+    navToggle.item(i).classList.toggle("hidden");
+  }
+};
+
 window.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar");
   if (window.scrollY > 50) {
-    navbar.classList.remove("transparent");
-    navbar.classList.add("scroll");
+    navbar.classList.add("bg-white", "shadow-2xl");
   } else {
-    navbar.classList.remove("scroll");
-    navbar.classList.add("transparent");
+    navbar.classList.remove("bg-white", "shadow-2xl");
   }
 });
+// // code for changing navbar bg color
+// window.addEventListener("scroll", function () {
+//   const navbar = document.getElementById("navbar");
+//   if (window.scrollY > 50) {
+//     navbar.classList.remove("transparent");
+//     navbar.classList.add("scroll");
+//   } else {
+//     navbar.classList.remove("scroll");
+//     navbar.classList.add("transparent");
+//   }
+// });
 
 //Back to top button
 // Get the button:
-let mybutton = document.getElementById("myButton");
+// let mybutton = document.getElementById("myButton");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
